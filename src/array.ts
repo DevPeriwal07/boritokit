ad5126b3b45de950d13d4adab205eb7fa5c3c8bf
Dev Periwal <96274188+DevPeriwal07@users.noreply.github.com>
2025-04-09 07:19:25 +0530

    fix: rename randInArr

--- src/array.ts
+++ src/array.ts
@@ -1,4 +1,4 @@
-export function randInArr<T>(arr: T[]): T | undefined {
+export function randomInArray<T>(arr: T[]): T | undefined {
   if (arr.length === 0) return undefined;
 
   return arr[Math.floor(Math.random() * arr.length)];
